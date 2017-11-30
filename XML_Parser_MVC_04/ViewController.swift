7889e1cf7ca4e68d73177613627a8e6ff6b67f39
RUSSELL.TSAI <vanilla.love10@gmail.com>
2017-11-30 19:51:33 +0800

    Initial Commit

---
+++ XML_Parser_MVC_04/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XML_Parser_MVC_04
+//
+//  Created by 蔡松樺 on 30/11/2017.
+//  Copyright © 2017 蔡松樺. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
